d06fd8634ad33d2fbb9813ef6d7ed4924f145e36
yyw <yyw99h@gmail.com>
2023-04-10 14:22:59 -0400

    add_tip

---
+++ server/routes/add_tip.js
@@ -0,0 +1,42 @@
+const { connection } = require("../db_connection")
+const { v4: uuidv4 } = require("uuid")
+
+async function addTip(req, res) {
+
+    const user_id = req.body.user_id
+    const business_id = req.body.business_id
+    const text = req.body.text
+
+    const tip_id = uuidv4()
+    const date = new Date()
+
+    connection.query(`
+        insert into tips
+        (
+            tip_id,
+            user_id,
+            business_id,
+            \`date\`,
+            \`text\`
+        )
+        values
+        (
+            ?, ?, ?, ?, ?, ?
+        )
+    `,
+        [tip_id, user_id, business_id, text],
+        (err, data) => {
+            if (err) {
+                res.json({
+                    error: err
+                })
+            } else {
+                res.json({
+                    tip_id: tip_id,
+                    date: date.toDateString(),
+                })
+            }
+        })
+}
+
+module.exports = addTip
