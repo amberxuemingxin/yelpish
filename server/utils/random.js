dba62dd7342741624e519f91c1bf965048dc61f0
Vopaaz <liyifan945@gmail.com>
2023-04-10 19:52:45 -0400

    Fix randomString generates string of wrong length

--- server/utils/random.js
+++ server/utils/random.js
@@ -1,7 +1,7 @@
 const { randomBytes } = require("node:crypto")
 
 function randomString(length) {
-    return randomBytes(length).toString('hex')
+    return randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length)
 }
 
 module.exports = { randomString }
