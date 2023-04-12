d04c30d1a5e488f176182fe70be4f3a80559aa36
Vopaaz <liyifan945@gmail.com>
2023-04-12 18:17:00 -0400

    Fix efficiency issue in recommendation

--- server/routes/recommendation.js
+++ server/routes/recommendation.js
@@ -1,58 +1,56 @@
 const { async_query } = require("../db_connection");
+const { getBusinessObjects } = require("../utils/business")
 
 async function recommendation(req, res) {
-  const longitude = parseFloat(req.query.longitude) || null;
-  const latitude = parseFloat(req.query.latitude) || null;
 
-  let query = `
-              SELECT
-              b.business_id AS id,
-              b.name,
-              GROUP_CONCAT(DISTINCT(c.category)) AS categories,
-              AVG(r.stars) AS rating
-    `;
+  const distance_valid = req.query.longitude && req.query.latitude
 
-  if (longitude && latitude) {
-    query += `
-        ,
-        ST_Distance_Sphere(
-          POINT(?,?),
-          POINT(b.longitude, b.latitude)
-      ) AS distance
-
-    `;
-  }
-
-  query += `
-    FROM business b
-        INNER JOIN business_categories c ON b.business_id = c.business_id
-        INNER JOIN review r ON b.business_id = r.business_id
-    WHERE
-        b.is_open = 1
-    GROUP BY
-        b.business_id
-    ORDER BY
-        rating DESC
-    LIMIT 10
-  `;
+  const longitude = parseFloat(req.query.longitude) ?? 0;
+  const latitude = parseFloat(req.query.latitude) ?? 0;
 
   try {
-    const businesses = await async_query(
-      query,
-      longitude && latitude ? [latitude, longitude, latitude] : []
-    );
+    const business_ids = (await async_query(
+      `
+        with business_rating as (
+          select
+            business_id,
+            avg(stars) as rating
+          from
+            review
+          group by
+            business_id
+        )
+        select
+          b.business_id
+        from
+          business b
+          inner join business_rating r
+          on b.business_id = r.business_id
+        order by
+          r.rating *
+          if( 
+            ?, 
+            st_distance_sphere(
+                point(longitude, latitude),
+                point(?, ?)
+            ), 
+            1
+          )
+          DESC
+        limit 10
+        ;
+      `
+      , [distance_valid, longitude, latitude]
+    )).map(obj => obj.business_id);
 
-    const formattedBusinesses = businesses.map((business) => {
-      return {
-        ...business,
-        categories: business.categories.split(','),
-        distance: longitude && latitude ? business.distance : null,
-      };
-    });
-
-    res.json({ businesses: formattedBusinesses });
+    res.json({
+      businesses: await getBusinessObjects(business_ids)
+    })
   } catch (err) {
-    res.status(500).json({ error: "Failed to retrieve recommendations.", errorString: err.toString() });
+    res.status(500).json({
+      error: "Failed to retrieve recommendations.",
+      errorString: err.toString(),
+    });
   }
 }
 
